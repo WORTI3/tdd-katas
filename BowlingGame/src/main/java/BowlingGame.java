3e60d00bc093c7b492a71d6d71711d4b4f853eea
Wortie <mc8atech@gmail.com>
2020-09-30 16:48:28 +0100

    BowlingGame Upload

--- BowlingGame/src/main/java/BowlingGame.java
+++ BowlingGame/src/main/java/BowlingGame.java
@@ -1,9 +1,35 @@
 public class BowlingGame {
 
     private final int MAX_PINS_PER_FRAME = 10;
-    private int score = 0;
+    private final int MAX_BOWLS_PER_GAME = 21;
 
-    public int score(){
+    private int roll = 0;
+    private int[] rolls = new int[MAX_BOWLS_PER_GAME]; // max
+
+    public void roll(int val){
+        rolls[roll++] = val;
+    }
+
+    public void roll(int...rolls){
+        for(int pins : rolls){
+            roll(pins);
+        }
+    }
+
+    public int score() {
+        int score = 0;
+        int i = 0; // index of frames
+        for(int frame = 0; frame < 10; frame++){
+            if(rolls[i] == MAX_PINS_PER_FRAME) { // Strike at first bowl
+                score += MAX_PINS_PER_FRAME + rolls[i+1] + rolls[i+2];
+                i++;
+            }else if(rolls[i] + rolls[i+1] == MAX_PINS_PER_FRAME){ // Checks for spare.. 2 parts of the frame (i and i+1)
+                score += MAX_PINS_PER_FRAME + rolls[i+2];
+            }else {
+                score += rolls[i] + rolls[i + 1];
+                i += 2;
+            }
+        }
         return score;
     }
 
